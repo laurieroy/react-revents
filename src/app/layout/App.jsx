1a4092eda375c1fc6cf16d3d37d07b522a3fc28e
lauretteroy <laurieroy.dev@gmail.com>
2022-09-10 09:18:54 -0700

    Add formOpen state

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,13 +1,17 @@
+import { useState } from "react";
+
 import EventDashboard from "../../features/events/eventDashboard/EventDashboard";
 import Navbar from "../../features/nav/Navbar";
 import { Container } from "semantic-ui-react";
 
 export default function App() {
+  const [formOpen, setFormOpen] = useState(false);
+
   return (
     <>
-      <Navbar />
+      <Navbar setFormOpen={setFormOpen} />
       <Container className="main">
-        <EventDashboard />
+        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} />
       </Container>
     </>
   );
