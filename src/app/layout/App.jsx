97905e62694bfa9d9770a15e73bb06af73efba58
lauretteroy <laurieroy.dev@gmail.com>
2022-09-12 09:51:18 -0700

    Add home page, styling, routing

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -23,13 +23,20 @@ export default function App() {
 
   return (
     <>
-      <Navbar setFormOpen={handleCreateFormOpen} />
-      <Container className="main">
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/events" component={EventDashboard} />
-        <Route path="/events/:id" component={EventDetails} />
-        <Route path="/createEvent" component={EventForm} />
-      </Container>
+      <Route exact path="/" component={HomePage} />
+      <Route
+        path={"/(.+)"}
+        render={() => (
+          <>
+            <Navbar setFormOpen={handleCreateFormOpen} />
+            <Container className="main">
+              <Route exact path="/events" component={EventDashboard} />
+              <Route path="/events/:id" component={EventDetails} />
+              <Route path="/createEvent" component={EventForm} />
+            </Container>
+          </>
+        )}
+      />
     </>
   );
 }
